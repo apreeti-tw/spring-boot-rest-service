b02f5a90fe75ece1a4428f657655730e935d3e1c
apreeti-tw <apreeti@thoughtworks.com>
2021-06-06 20:39:43 +0530

    Add test when book does not exist

--- src/test/java/com/springbootfundamentals/springbootrestservice/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/springbootfundamentals/springbootrestservice/SpringBootRestServiceApplicationTests.java
@@ -1,15 +1,32 @@
 package com.springbootfundamentals.springbootrestservice;
 
+import com.springbootfundamentals.springbootrestservice.controller.Library;
+import com.springbootfundamentals.springbootrestservice.controller.LibraryController;
+import com.springbootfundamentals.springbootrestservice.repositories.ILibraryRepository;
 import com.springbootfundamentals.springbootrestservice.service.LibraryService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SpringBootRestServiceApplicationTests {
 	@Autowired
 	LibraryService libraryService;
+
+	@Autowired
+	LibraryController libraryController;
+
+	@MockBean
+	LibraryService libService;
+
+	@MockBean
+	ILibraryRepository libraryRepository;
 
 	@Test
 	void contextLoads() {
@@ -25,5 +42,25 @@ class SpringBootRestServiceApplicationTests {
 	public void checkBookIdGeneratedWhenBookIsNew(){
 		String bookId = libraryService.getId("ISBN", 5566);
 		Assertions.assertEquals(bookId, "ISBN5566");
+	}
+
+	@Test
+	public void testAddBookWhenBookIdDoesNotExist(){
+		Library bookDetails = buildLibraryBook();
+		when(libService.getId(bookDetails.getIsbn(), bookDetails.getAisle())).thenReturn(bookDetails.getId());
+		when(libService.checkDuplicateBook(bookDetails.getId())).thenReturn(false);
+
+		ResponseEntity responseEntity = libraryController.addBookImpl(bookDetails);
+		Assertions.assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
+	}
+
+	public Library buildLibraryBook(){
+		Library bookDetails = new Library();
+		bookDetails.setBookName("Unit Testing Book");
+		bookDetails.setAuthor("Unit Test");
+		bookDetails.setAisle(2245);
+		bookDetails.setIsbn("ISBN6");
+		bookDetails.setId("ISBN62245");
+		return bookDetails;
 	}
 }
