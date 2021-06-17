cc9e8b38750e059e4da619e46687ed315467244d
apreeti-tw <apreeti@thoughtworks.com>
2021-06-17 14:38:22 +0530

    Add data source to application-test.properties file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO books(book_name,id,isbn,aisle,author) values('Cypress','abcd4','abcd','4','Rahul');
-INSERT INTO books(book_name,id,isbn,aisle,author) values('Devops','fdsefr343','fdsefr3','43','Rahul');
-INSERT INTO books(book_name,id,isbn,aisle,author) values('Devops','ddy22','ddy','22','Shetty');
+INSERT INTO books(book_name,id,isbn,aisle,author) VALUES('Cypress','abcd4','abcd','4','Rahul');
+INSERT INTO books(book_name,id,isbn,aisle,author) VALUES('Devops','fdsefr343','fdsefr3','43','Rahul');
+INSERT INTO books(book_name,id,isbn,aisle,author) VALUES('Devops','ddy22','ddy','22','Shetty');
